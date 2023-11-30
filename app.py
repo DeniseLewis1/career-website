deefe8c40062f93694859e06c0aa59e555fb0674
DeniseLewis1 <121467173+DeniseLewis1@users.noreply.github.com>
2023-11-30 20:27:39 +0000

    Use Jinja template to display job listings

--- app.py
+++ app.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Los Angeles, CA",
+    "salary": "$80,000"
+  },
+  {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "San Francisco, CA",
+    "salary": "$150,000"
+  },
+  {
+    "id": 3,
+    "title": "Frontend Engineer",
+    "location": "Remote"
+  },
+  {
+    "id": 4,
+    "title": "Backend Engineer",
+    "location": "Remote",
+    "salary": "$120,000"
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
